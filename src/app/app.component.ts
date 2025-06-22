573ff3092345c10b1588bafc5251560b4f3f3467
Miguel <mgroyes22@gmail.com>
2025-06-22 12:02:29 -0500

    comentario inicial Angular landing page Bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { FeaturesComponent } from './components/features/features.component';
+import { PricingComponent } from './components/pricing/pricing.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { FormComponent } from "./components/form/form.component";
+import { TestimonialsComponent } from "./components/testimonials/testimonials.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, CommonModule, RouterOutlet, HeaderComponent, HeroComponent, FeaturesComponent, PricingComponent, FooterComponent, FormComponent, TestimonialsComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
